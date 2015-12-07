2c2a8aea059cf944f3acb18970b8ef16cd03b61d
egoist <0x142857@gmail.com>
2015-12-07 12:01:46 +0800

    fix args

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export default function ({
   chars = ['★', '☆'],
   rating = 0
-}) {
+} = {}) {
   const positivePoints = chars[0].repeat(parseInt(rating));
   const afterDot = (rating % 1 >= 0.5) ? chars[1] : '';
   return positivePoints + afterDot;
